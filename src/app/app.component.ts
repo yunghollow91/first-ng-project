530a31f1110e394fc98b34717e5e6738aa515357
yunghollow91 <vutrungnguyen@live.de>
2022-06-28 22:25:55 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  private list = new TodoList('Bob', [
+    new TodoItem('Go for run', true),
+    new TodoItem('Get Flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter((item) => !item.complete).length;
+  }
+}
